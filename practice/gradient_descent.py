6f90011a75ca907d8a87cfa38c9ddb51239389da
aarnav-11 <aarnav11@g.ucla.edu>
2026-02-07 15:37:26 -0800

    Added first part of gradient descent

--- practice/gradient_descent.py
+++ practice/gradient_descent.py
@@ -1,26 +1,21 @@
-import warnings
-warnings.filterwarnings("ignore")
+
 import random
 import numpy as np
 import pandas as pd
-pd.set_option('display.max_columns',100)
-import seaborn as sns
 import matplotlib.pyplot as plt
-plt.rcParams['font.size'] = 14
-plt.rcParams['figure.figsize'] = (22, 5)
-plt.rcParams['figure.dpi'] = 100
-import sqlite3
-from sklearn.metrics import r2_score, roc_curve, auc
-from urllib.request import urlretrieve
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
-from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import train_test_split
 
+dataset_url = "Housing.csv"
+housing_df = pd.read_csv(dataset_url)
 
-housing_dataset = "Housing.csv"
-housing_df = pd.read_csv(housing_dataset)
+housing_df = housing_df[['price', 'area']] #select just the price and area
 
-print(housing_df.head())
+# #visualizing the data
+# plt.scatter(housing_df['area'], housing_df['price'])
+# plt.show()
+# print(housing_df.head())
+n, m = np.shape(housing_df)
 
-# print(medical_df.shape) #1338 by 7 so 7 features and 1338 datapoints
+w_tilde = np.zeros(m+1)
 
+for price, area in housing_df:
+    w_tilde = w_tilde
